7088cf058bee8ccd30d2e178af4eef60680a23ee
Mon-Lay <49906170+Mon-Lay@users.noreply.github.com>
2020-08-03 16:59:12 +0800

    Add Class for Question D No.2

--- D_Solution2_Test.java
+++ D_Solution2_Test.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class D_Solution2_Test {
 
 	@Test 
-	public void caterpillar(){
+	public void caterpillar() {
 		Caterpillar caterpillar = new Caterpillar();
 		assertEquals(false, caterpillar.fly);
 		assertEquals(true, caterpillar.walk);
